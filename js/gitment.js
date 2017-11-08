6bd2cc772f1ba60efb85bf43e5559d33344f94a6
shujia.wu <shujia.wu@huochaicf.com>
2017-11-08 13:58:12 +0800

    Site updated: 2017-11-08 13:58:12

---
+++ js/gitment.js
@@ -0,0 +1,9 @@
+var gitment = new Gitment({
+  owner: 'ShujiaWu',
+  repo: 'ShujiaWu.github.io',
+  oauth: {
+    client_id: 'e87a36369b1bed79f408',
+    client_secret: '672bb0956ec56c2634d20fcb4a06b65ddabc6fae',
+  },
+})
+gitment.render('container')
